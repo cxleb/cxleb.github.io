4204181f2ab3c5494cce6f4790ca2b51a20293c6
Caleb <waitcaleb@gmail.com>
2019-07-15 17:20:54 +1000

    more

--- res/projects.js
+++ res/projects.js
@@ -1,4 +1,9 @@
 var projects = {
+  "avsketch" : {
+    "title" : "AVSketch",
+    "desc" : "A vector drawing application with an infinite canvas used to sketch up an idea as if you would in a notebook, made for my HSC SDD 'Major' project. you can view it on github",
+    "link" : "https://github.com/cxleb/AVSketch"
+  },
   "to10" : {
     "title" : "CityRail to 10 Cheat",
     "desc" : "When on CityRail trains, a game you play is: get the 4 numbers on the carriage id and make them add to 10. I was lazy and being a software developer I made a web app to solve my problem, written in js it runs in the web",
@@ -11,7 +16,7 @@ var projects = {
   },
   "invasion" : {
     "title" : "Random Map Generation Graphical Demo",
-    "desc" : "Originally meant to be a game, instead it is a graphical demo showing a perlin noise like generating random maps, graphics done in a custom WebGL graphics engine using glmatrix and as math supporter. all models designed by me",
+    "desc" : "Originally meant to be a game, instead it is a graphical demo showing a perlin-noise-like map generation, graphics done in a custom WebGL graphics engine using glmatrix and as math supporter. all models designed by me",
     "link" : "jsapps/invade/index.html"
   }
 }
